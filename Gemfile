7865104e6706924a341c8026ebd23cf29107f640
Klaus Meyer <spam@klaus-meyer.net>
2022-12-18 10:22:42 +0100

    add bootstrap and basic styling

--- Gemfile
+++ Gemfile
@@ -43,11 +43,13 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 
 # Use Sass to process CSS
-# gem "sassc-rails"
+gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem "bootstrap"
+gem "jquery-rails"
 gem "rails-i18n"
 
 group :development, :test do
@@ -65,4 +67,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,23 @@ user = User.first_or_create do |u|
   u.daily_working_hours = 8.0
 end
 
+# Entries::Base.delete_all
+
 if user.entries.none? && ENV['DUMMY_ENTRIES'].present?
   user.entries << Entries::Manual.new(date: '2021-12-31', time_manual: 23.0)
 
   user.entries << Entries::General.new(date: '2022-10-31', start_time: '08:59', end_time: '17:29')
   user.entries << Entries::Holiday.new(date: '2022-11-01')
   user.entries << Entries::General.new(date: '2022-11-02', start_time: '08:59', end_time: '17:29')
-  user.entries << Entries::General.new(date: '2022-11-03', start_time: '08:59', end_time: '17:59')
+  user.entries << Entries::SickLeave.new(date: '2022-11-03')
   user.entries << Entries::General.new(date: '2022-11-04', start_time: '09:59', end_time: '17:29')
 
   user.entries << Entries::General.new(date: '2022-11-07', start_time: '08:59', end_time: '17:29')
-  user.entries << Entries::General.new(date: '2022-11-08', start_time: '08:59', end_time: '17:29')
-  user.entries << Entries::General.new(date: '2022-11-09', start_time: '08:59', end_time: '17:29')
+  user.entries << Entries::Vacation.new(date: '2022-11-08')
+  user.entries << Entries::Vacation.new(date: '2022-11-09')
   user.entries << Entries::General.new(date: '2022-11-10', start_time: '08:59', end_time: '17:29')
   user.entries << Entries::Flextime.new(date: '2022-11-11')
+
+  user.entries << Entries::General.new(date: '2022-12-05', start_time: '08:59', end_time: '17:29')
+  user.entries << Entries::General.new(date: '2022-12-06', start_time: '08:59', end_time: '17:29')
 end
