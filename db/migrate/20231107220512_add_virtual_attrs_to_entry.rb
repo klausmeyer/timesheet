db63e464c3c390d53b44bc6f96097b79ec41f01d
Klaus Meyer <spam@klaus-meyer.net>
2023-11-07 23:17:53 +0100

    use rails 7.1 / pg virtual attributes

---
+++ db/migrate/20231107220512_add_virtual_attrs_to_entry.rb
@@ -0,0 +1,6 @@
+class AddVirtualAttrsToEntry < ActiveRecord::Migration[7.1]
+  def change
+    add_column :entries, :week, :virtual, type: :string, as: "DATE_PART('week', date)", stored: true
+    add_column :entries, :year, :virtual, type: :string, as: "DATE_PART('year', date)", stored: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_094829) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_07_220512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_094829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.virtual "week", type: :string, as: "date_part('week'::text, date)", stored: true
+    t.virtual "year", type: :string, as: "date_part('year'::text, date)", stored: true
     t.index ["deleted_at"], name: "index_entries_on_deleted_at"
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
